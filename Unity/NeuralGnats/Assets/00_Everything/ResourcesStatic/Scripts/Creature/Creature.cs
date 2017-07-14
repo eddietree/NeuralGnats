4795d4738efeac6c6c5e9c699ad553f6bce0d7c6
eddietree <eddie@funktroniclabs.com>
2017-07-13 17:40:09 -0700

    neural netwss

--- Unity/NeuralGnats/Assets/00_Everything/ResourcesStatic/Scripts/Creature/Creature.cs
+++ Unity/NeuralGnats/Assets/00_Everything/ResourcesStatic/Scripts/Creature/Creature.cs
@@ -13,14 +13,20 @@ public class Creature : MonoBehaviour {
     Rigidbody2D rb;
 
     const int numFeelers = 5;
+    const float angleSpreadDegrees = 110.0f;
+    const float feelerDist = 2.0f;
+
+    public float[] feelerDanger = new float[numFeelers];
+    public float[] feelerHunger = new float[numFeelers];
+
+    public float[] neuralNetInput;
+    public float[] neuralNetOutput;
 
     NeuralNetwork neuralNet;
     
     void Start ()
     {
-        int[] layerSizes = new int[] { 3, 10, 10, 2 };
-
-        neuralNet = new NeuralNetwork(layerSizes);
+        InitNeuralNetwork();
 
         rb = GetComponent<Rigidbody2D>();
 
@@ -31,17 +37,129 @@ public class Creature : MonoBehaviour {
         StartCoroutine(HandleSteering());	
 	}
 
+    void InitNeuralNetwork()
+    {
+        // feelers, angle, velocity, angular velocity
+        int numInputs = feelerDanger.Length + feelerHunger.Length + 3;
+        int numOutputs = 2;
+
+        neuralNetInput = new float[numInputs];
+        neuralNetOutput = new float[numOutputs];
+
+        for (int i = 0; i < neuralNetInput.Length; ++i)
+            neuralNetInput[i] = 0.0f;
+
+        // neural nets
+        int[] layerSizes = new int[] { numInputs, 20, 20, numOutputs };
+        neuralNet = new NeuralNetwork(layerSizes);
+    }
+
     private void Update()
     {
-        DrawDebugLines();
+        UpdateFeelers();
+        //DrawDebugLines();
+    }
+
+    void UpdateFeelers()
+    {
+        var layerMaskObstacle = LayerMask.GetMask("Obstacle");
+        var layerMaskFood = LayerMask.GetMask("Food");
+
+        var angleMin = -angleSpreadDegrees;
+        var angleDelta = (angleSpreadDegrees * 2.0f) / (numFeelers - 1.0f);
+
+        var rayOrigin = transform.position;
+
+        var resultHitFood = Physics2D.CircleCast(rayOrigin, feelerDist, transform.forward, feelerDist, layerMaskFood);
+
+        var dirToFood = Vector3.zero;
+        var distToFood = 0.0f;
+
+        if (resultHitFood)
+        {
+            var collisionPoint = resultHitFood.collider.transform.
+                position;
+
+            dirToFood = collisionPoint - rayOrigin;
+            distToFood = dirToFood.magnitude;
+
+            Debug.DrawLine(rayOrigin, collisionPoint, Color.yellow);
+        }
+
+        for (int iFeeler = 0; iFeeler < numFeelers; ++iFeeler)
+        {
+            var angle = angleMin + angleDelta * iFeeler;
+            var lineDir = Quaternion.Euler(0.0f, 0.0f, angle) * transform.up;
+            var rayDirection = lineDir;
+
+            var resultHitObstacle = Physics2D.Raycast(rayOrigin, rayDirection, feelerDist, layerMaskObstacle);
+
+            if (resultHitObstacle)
+            {
+                // contact!
+                Debug.DrawLine(rayOrigin, rayOrigin + rayDirection * resultHitObstacle.distance, Color.red);
+
+                feelerDanger[iFeeler] = 1.0f - resultHitObstacle.distance / feelerDist;
+            }
+            else
+            {
+                // no contact!
+                Debug.DrawLine(rayOrigin, rayOrigin + rayDirection * feelerDist, Color.blue);
+
+                feelerDanger[iFeeler] = 0.0f;
+            }
+
+            // hit food
+            if (resultHitFood)
+            {
+                var angleBetween = Vector3.Angle(dirToFood, rayDirection);
+
+                var angleMax = angleDelta;
+                var angleWeight = 1.0f- Mathf.Clamp01(angleBetween / angleMax);
+
+                //feelerHunger[iFeeler] = angleWeight;
+
+                feelerHunger[iFeeler] = Mathf.Clamp01((1.0f- distToFood/feelerDist) * angleWeight);
+            }
+            else
+            {
+                feelerHunger[iFeeler] = 0.0f;
+            }
+        }
+    }
+
+    void UpdateNeuralNetOutput()
+    {
+        for(int i = 0; i < numFeelers; ++i)
+        {
+            neuralNetInput[i] = feelerDanger[i];
+            neuralNetInput[i + numFeelers] = feelerHunger[i];
+        }
+
+        var fwd = transform.up;
+
+        neuralNetInput[numFeelers * 2+0] = rb.velocity.magnitude;
+        neuralNetInput[numFeelers * 2+1] = rb.angularVelocity;
+        neuralNetInput[numFeelers * 2+2] = Mathf.Atan2(fwd.y, fwd.x);
+
+        // feed forward
+        neuralNet.FeedForward(neuralNetInput, neuralNetOutput);
     }
 
     void DrawDebugLines()
     {
         var lineStart = transform.position;
-        var lineDir = transform.up;
+        
+        var angleMin = -angleSpreadDegrees;
+        var angleDelta = (angleSpreadDegrees * 2.0f) / (numFeelers-1.0f);
 
-        Debug.DrawLine(lineStart, lineStart + lineDir, Color.red);
+        for (int iFeeler = 0; iFeeler < numFeelers; ++iFeeler)
+        {
+            var angle = angleMin + angleDelta * iFeeler;
+            var lineDir = Quaternion.Euler(0.0f, 0.0f, angle) * transform.up;
+
+            Debug.DrawLine(lineStart, lineStart + lineDir * feelerDist, Color.red);
+        }
     }
 
     CreatureThruster CreateThruster(Transform parent)
@@ -59,15 +177,15 @@ public class Creature : MonoBehaviour {
     {
         while(true)
         {
+            UpdateNeuralNetOutput();
+
             Vector2 fwd = transform.up;
             Vector2 left = fwd;
             Vector2 right = fwd;
 
-            //rb.AddForce(fwd*0.1f);
-
             var forceScalar = 0.1f;
 
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) )
             {
                 rb.AddForceAtPosition(left * forceScalar, thrusterLeft.transform.position);
                 thrusterLeft.EmitParticles(1);
@@ -86,6 +204,24 @@ public class Creature : MonoBehaviour {
                 rb.velocity = Vector2.zero;
                 rb.angularVelocity = 0.0f;
             }
+
+            var forceLeft = neuralNetOutput[0];
+            var forceRight = neuralNetOutput[1];
+
+            if (forceLeft > 0.0f)
+            {
+                rb.AddForceAtPosition(left * forceScalar * forceLeft, thrusterLeft.transform.position);
+                thrusterLeft.EmitParticles(1);
+            }
+
+            if (forceRight > 0.0f)
+            {
+                rb.AddForceAtPosition(right * forceScalar * forceRight, thrusterRight.transform.position);
+                thrusterRight.EmitParticles(1);
+            }
+
+            UpdateNeuralNetOutput();
+
 
             yield return null;
         }
